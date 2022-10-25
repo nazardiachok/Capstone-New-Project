d317c5f6e7822b7db86315c798b7f0ffe43fdddb
Nazar Diachok <diafcc55@gmail.com>
2022-10-25 12:41:53 +0200

    -Page Warenkorb erstellt -diese mit Navigation verbunden

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navigation from "./components/Navigation";
 import AppHeader from "./components/Header";
 import HomePage from "./pages/homePage";
 import { Routes, Route } from "react-router-dom";
+import Warenkorb from "./pages/Warenkorb";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <AppHeader />
       <Routes>
         <Route path="/home" element={<HomePage />}></Route>
+        <Route path="/warenkorb" element={<Warenkorb />}></Route>
       </Routes>
 
       <Navigation />
---
+++ src/pages/Warenkorb.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+export default function Warenkorb() {
+  return (
+    <WKorb>
+      <h1>Warenkorb</h1>
+    </WKorb>
+  );
+}
+const WKorb = styled.div`
+  display: flex;
+  justify-content: center;
+`;
