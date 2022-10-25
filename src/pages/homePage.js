81ede70de5e3c462efea0b68389a5f45653e5aa4
Nazar Diachok <diafcc55@gmail.com>
2022-10-25 14:50:40 +0200

    -inpun and select fields created in HomePage

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,3 +1,26 @@
+import styled from "styled-components";
 export default function HomePage() {
-  return <>HomePAge</>;
+  return (
+    <>
+      <SectionInput>
+        <div>
+          <input type="text" />
+          <form action="">
+            <select>
+              <option value="">Category</option>
+              <option value="Herrenschuh">Herrenschuh</option>
+              <option value="Dammenschuh">Dammenschuh</option>
+            </select>
+          </form>
+        </div>
+      </SectionInput>
+    </>
+  );
 }
+const SectionInput = styled.section`
+  margin-top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: -10;
+`;
