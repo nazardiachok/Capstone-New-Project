123c559b1350142ceaecbb3413ff099662e735dc
Nazar Diachok <diafcc55@gmail.com>
2022-10-25 12:51:43 +0200

    -Data.js created -data array added

---
+++ src/components/Data.js
@@ -0,0 +1,148 @@
+const data = {
+  dataItems: [
+    {
+      id: 1,
+      title: "Nike Air Force 1 '07",
+      category: "Herrenschue",
+      MadeIn: "China",
+      price: 119,
+      image:
+        "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/b7d9211c-26e7-431a-ac24-b0540fb3c00f/air-force-1-07-herrenschuh-GjGXSP.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 2,
+      title: "Air Jordan 1 Mid",
+      category: "Damenschuh",
+      MadeIn: "China",
+      price: 129,
+      image:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/48f2834b-bc0a-4fd6-8513-33d3ff0e7489/air-jordan-1-mid-damenschuh-XrWJMF.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 3,
+      title: "Nike Air Force 1 '07 LV8",
+      category: "Herrenschue",
+      MadeIn: "China",
+      price: 117,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/ed4fb193-247d-4e4e-aaf7-12ff56bdffd8/air-force-1-07-lv8-herrenschuh-kLnMBL.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 4,
+      title: "Nike Air Force 1 React",
+      category: "Herrenschuh",
+      MadeIn: "China",
+      price: 134,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/a6bce3ef-bc25-4e3d-b45b-db8223745a49/air-force-1-react-herrenschuh-G09PQp.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 5,
+      title: "Nike Air Zoom Alphafly NEXT% 2",
+      category: "Herrenschuh",
+      MadeIn: "China",
+      price: 299,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/928f082e-4b98-4fb5-9eb2-13929291857e/air-zoom-alphafly-next-2-herren-strassenlaufschuh-fur-wettkampfe-fvDSdT.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 6,
+      title: "Nike Air Force 1 '07 LX",
+      category: "Damenschuh",
+      MadeIn: "Vietnam",
+      price: 119,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/b73a2874-5e78-4ece-ae2d-44855da7210c/air-force-1-07-lx-schuhe-fur-Wn761w.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 7,
+      title: "Nike React Infinity Run Flyknit 3",
+      category: "Herrenschuh",
+      MadeIn: "Vietnam",
+      price: 107,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/db03a7f6-5d72-4a93-82b9-b0c7fb56bc0c/react-infinity-run-flyknit-3-strassenlaufschuh-fur-V2tvkX.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 8,
+      title: "Nike Air Huarache By You",
+      category: "Damenschuh",
+      MadeIn: "Vietnam",
+      price: 159,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/23148737-cf6e-4453-b7f2-9cc75a0365fe/custom-huarache-run-by-you.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 9,
+      title: "Nike Jordan",
+      category: "Herrenschuh",
+      MadeIn: "Vietnam",
+      price: 99,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/017adbe2-656c-497f-a913-d28687a52a8d/zion-2-herren-basketballschuh-khWbrw.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 10,
+      title: "Nike Jordan 1 Mid Alt SE",
+      category: "Damenschuh",
+      MadeIn: "China",
+      price: 129,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/7b8812d8-ae29-4dad-87c8-d4691cc77ce2/jordan-1-mid-alt-se-schuh-fur-jungere-PvNJKb.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 11,
+      title: "Nike Air Force 1 PLT.AF.ORM",
+      category: "Damenschuh",
+      MadeIn: "Vietnam",
+      price: 149,
+      image:
+        "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/618fdea5-d1d8-4afa-b219-d929a9ec29f1/air-force-1-pltaform-damenschuh-T8k2JB.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 12,
+      title: "Nike Jordan KD15",
+      category: "Damenschuh",
+      MadeIn: "Vietnam",
+      price: 169,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/a7c9d0b9-3a90-4ab5-a874-3a5f36ddfd44/kd15-basketballschuh-dltSlN.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+    {
+      id: 13,
+      title: "Nike Air Zoom Pegasus 39 Shield",
+      category: "Damenschuh",
+      MadeIn: "Vietnam",
+      price: 134,
+      image:
+        "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/202de7ea-40c2-4785-864f-6efb74f08787/air-zoom-pegasus-39-shield-wetterfester-strassenlaufschuh-fur-2TjRx0.png",
+      details:
+        "Die revolutionäre Air-Technologie kam im Jahr 1978 zum ersten Mal in Schuhen von Nike zum Einsatz. 1987 feierte dann der Air Max 1 sein Debüt – mit sichtbarer Air-Technologie in der Ferse. Plötzlich war der Tragekomfort der Air-Dämpfung nicht mehr nur spürbar, sondern auch zu sehen. Seitdem sind Air Max-Schuhe der nächsten Generation mit ihren auffälligen Farbkombinationen und der zuverlässigen, leichten Dämpfung sowohl bei Athlet:innen als auch bei Sammler:innen sehr beliebt.",
+    },
+  ],
+};
+export default data;
