ae10f35110f2d1cea5c142cc8d7d35607e0c2e56
Nazar Diachok <diafcc55@gmail.com>
2022-10-27 11:56:09 +0200

    -details page becam props and filter -in App.js and homePage  path adapted

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,11 +1,24 @@
 import { useParams } from "react-router-dom";
 
-export default function Details() {
+import { useNavigate } from "react-router-dom";
+
+export default function Details({ output }) {
+  const navigate = useNavigate();
+
   const { id } = useParams();
+  const details = output.filter((obj) => obj.id === +id);
+
+  console.log(output);
+  console.log(details);
+  console.log(id);
+
   return (
     <>
       <h1>Details</h1>
-      <h1></h1>
+      <h1>Details</h1>
+      <div>{details[0].title}</div>
+
+      <button onClick={() => navigate("/home")}>return</button>
     </>
   );
 }
